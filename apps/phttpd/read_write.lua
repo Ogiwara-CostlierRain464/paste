37887d707ade2dece9f0daa26e68131eb659081f
ogiwara <yushiogiwara@gmail.com>
2022-09-28 12:17:10 +0100

    add flexible query

---
+++ apps/phttpd/read_write.lua
@@ -0,0 +1,11 @@
+wrk.method = "POST"
+s = "r 0\n"
+wrk.body = s
+for i = 1, 1 do
+	wrk.body = wrk.body.."r "..i.."\n"
+end
+for i = 1, 1 do
+	wrk.body = wrk.body.."w "..i.." "..i.."\n"
+end
+wrk.body = wrk.body.."r 4"
+wrk.headers["Content-Type"] = "application/x-www-form-urlencoded"
